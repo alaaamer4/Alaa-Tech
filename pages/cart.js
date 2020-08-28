f46b67388952f10d28078cd3ff0d49431496594d
iam_alaa <alaaeldine1996@gmail.com>
2020-08-28 12:39:32 +0200

    adjasting side cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,8 +1,14 @@
 import Hero from "../components/utils/Hero";
+import * as types from "../store/types";
+import { useEffect } from "react";
 import Layout from "../components/utils/Layout";
 import img from "../images/store.jpg";
 import Cart from "../components/cart/Cart";
+import { useDispatch } from "react-redux";
 export default function CartPage() {
+  useEffect(() => {
+    useDispatch({ type: types.CLOSE_SIDE_CART });
+  }, []);
   return (
     <Layout>
       <Hero img={img} />
